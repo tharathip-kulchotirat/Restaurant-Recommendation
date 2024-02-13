f2737fc6e6778b7cfda081176cc99b09643a1fde
@tharathip.ku <83084032+tharathip-kulchotirat@users.noreply.github.com>
2024-02-14 00:38:38 +0700

    checkpoint: Change Query and logics

--- app/api/api_recommendation.py
+++ app/api/api_recommendation.py
@@ -4,7 +4,10 @@ from fastapi.responses import ORJSONResponse
 import mlflow
 from geopy.distance import geodesic
 import os
-from sqlalchemy import select
+# NEW: import sqlalchemy
+from sqlalchemy import select, func, cast, Float
+# END NEW
+
 import numpy as np
 
 # for generating request_id
@@ -105,41 +108,58 @@ async def recommend(
         raise HTTPException(status_code=404, detail="User not found")
 
     # Inference user features to Nearest Neighbors model
-    user_features = np.array([getattr(user, f"feature_{i}") for i in range(1000)]).reshape(1, -1)        
-    difference, indices = model.kneighbors(user_features, n_neighbors=request.size)
+    user_features = np.array([getattr(user, f"feature_{i}") for i in range(1000)]).reshape(1, -1)
+    
+    # NEW: EDIT get restaurants that are within the max_dis
+    # install `brew install postgis` first, then...
+    # SQL: CREATE EXTENSION postgis;
+    # SQL: SELECT PostGIS_Version();
+    restaurants = await db.execute(select(Restaurant.restaurant_id, Restaurant.latitude, Restaurant.longitude)
+                                    .filter(
+                                        func.ST_DistanceSphere(
+                                            func.ST_MakePoint(request.latitude, request.longitude),
+                                            func.ST_MakePoint(Restaurant.latitude, Restaurant.longitude)
+                                        ) <= float(request.max_dis)
+                                    ))
+    restaurants = restaurants.all()
+    restaurants = [(row[0], int(geodesic((request.latitude,request.longitude), (row[1], row[2])).meters)) for row in restaurants if int(geodesic((request.latitude,request.longitude), (row[1], row[2])).meters) <= request.max_dis]
+    # END NEW
+    
+    # NEW: find nn from len of restaurant_ids
+    difference, indices = model.kneighbors(user_features, n_neighbors=len(restaurants))
+    sorted_nn = sorted(zip(difference[0], indices[0]), key=lambda x: x[0])
+    indices = [idx for _, idx in sorted_nn]
+    difference = [diff for diff, _ in sorted_nn]
+    # END NEW
+    
     # NOTE: Padding the indices need to be 4 digits rather than 5 (according to the example in assignment instruction). This is because the restaurant_id in the database is 4 digits.
-    padded_array = [[f'{num:04}' for num in row] for row in indices]
+    padded_array = [f'{row:04}' for row in indices]
 
     # Calculate the displacement between user and restaurants
-    restaurant_idx = [(f"r{idx}", diff) for idx, diff in zip(padded_array[0], difference[0])]
-    # Extract restaurant ids
-    restaurant_ids = np.array([r[0] for r in restaurant_idx])
-    restaurants = await db.execute(select(Restaurant).filter(Restaurant.restaurant_id.in_(restaurant_ids)))
-    restaurants = restaurants.scalars().all()
-    restaurant_dict = {restaurant.restaurant_id: restaurant for restaurant in restaurants}
+    restaurant_idx = [(f"r{idx}", diff) for idx, diff in zip(padded_array, difference)]
     
+    # NEW
     recommended_restaurants = []
     for r_idx, diff in restaurant_idx:
-        restaurant = restaurant_dict.get(r_idx)
-        if restaurant:
-            coords_user = (request.latitude, request.longitude)
-            coords_restaurant = (restaurant.latitude, restaurant.longitude)
-            displacement = int(geodesic(coords_user, coords_restaurant).m)
-            if displacement <= int(request.max_dis):
-                recommended_restaurants.append(
-                    Recommendation(
-                        id=r_idx,
-                        difference=round(float(diff), 1),
-                        displacement=displacement
-                    )
+        ids = [x[1] for x in restaurants if x[0] == r_idx]
+        if len(ids) > 0:
+            recommended_restaurants.append(
+                Recommendation(
+                    id=r_idx,
+                    difference=round(float(diff), 1),
+                    displacement=ids[0]
                 )
-    # Sort the recommended restaurants by displacement
+        )
+    # END NEW
+    
+    # # Sort the recommended restaurants by displacement
     if bool(request.sort_dis):
         # Ascending order of displacement
         recommended_restaurants = sorted(recommended_restaurants, key=lambda x: x.displacement, reverse=False)
     else:
         # Ascending order of difference
         recommended_restaurants = sorted(recommended_restaurants, key=lambda x: x.difference, reverse=False)
+    
         
     # Save prediction artifacts and request_params to the database
     # request_params
